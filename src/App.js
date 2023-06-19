925b1adde74367e10aa41d6df1bbdd34d1c0898a
Vivek <veeveckjeet@gmail.com>
2023-06-19 19:54:31 -0400

    Small commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
+import React ,{useState} from 'react';
 
 function App() {
+  const [name,setName]=useState("Vivi");
+  const [date,setDate]=useState("DOB");
+  const handleNameChange=(e)=>{
+     setName(e.target.value);
+  }
+  const handleDateChange=(e)=>{
+     setDate(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+     <h1 className="text-center">Signature App</h1>
+     <Title text={name}/>
+     <Title text={date}/>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis itaque perspiciatis similique est, quibusdam accusantium rem velit culpa repellat vero illum, voluptate, tempore vel voluptatibus maiores corrupti! Numquam, tenetur placeat!</p>
+     <div className='d-flex input-boxes'>
+      <input type="date" value={date} onChange={handleDateChange}/>
+
+      <input type="text" value={name} onChange={handleNameChange}/>
+     </div>
     </div>
   );
 }
